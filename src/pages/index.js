f1ccbefc977cbe9c0de849d3769011f1204b0a97
Ben Naylor <nayben@gmail.com>
2019-07-07 21:04:32 +0200

    feat(routes): Automatic language selection and routing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,34 @@
 import React from 'react'
-import { graphql, Link } from 'gatsby'
-import Helmet from 'react-helmet'
-import Layout from '../components/layout'
-import MealPreview from '../components/MealPreview'
-import styled from 'styled-components'
+import { graphql, navigate, withPrefix } from 'gatsby'
+import { getUserLangKey } from 'ptz-i18n'
 
-const ItemWrapper = styled.div`
-  display: flex;
-  padding: 20px;
-  overflow-x: hidden;
-  flex-wrap: wrap;
-  justify-content: space-between;
-`
+class RouteIndex extends React.Component {
+  constructor(props) {
+    super(props)
 
-class RootIndex extends React.Component {
+    if (typeof window !== undefined) {
+      const { languages, defaultLocale } = props.data.site.siteMetadata.locales;
+      const langKey = getUserLangKey(languages, defaultLocale);
+      navigate(withPrefix(`/${langKey}`))
+    }
+  }
+
   render() {
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const { languages } = this.props.data.site.siteMetadata.locales
-
     return (
-      <Layout location={this.props.location} >
-        <div style={{ background: '#fff' }}>
-          {languages.map(language => (
-            <Link to={`/${language}/`}>{language}</Link>
-          ))}
-        </div>
-      </Layout>
+      <div />
     )
   }
 }
 
-export default RootIndex
+export default RouteIndex
 
 export const pageQuery = graphql`
-  query HomeQuery2 {
+  query RouteQuery {
     site {
       siteMetadata {
-        title
         locales {
           languages
+          defaultLocale
         }
       }
     }
