6a6efd8cdd0edbc1a8cf152246c5bc5dc2d2e50b
Ben Naylor <nayben@gmail.com>
2019-07-07 21:48:28 +0200

    feat(nav): reducing complexity

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,28 +1,45 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import Container from './container'
-import Navigation from './navigation'
+import styled from 'styled-components'
 
 import './base.css'
 import 'normalize.css'
 
-class Template extends React.Component {
+const Container = styled.div`
+  max-width: 1180px;
+  margin: 0 auto;
+`
+
+const Navigation = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  height: 20vh;
+  max-height: 100px;
+  font-size: 1.25rem;
+`
+
+const NavigationItem = styled.div`
+  display: inline-flex;
+  align-items: center;
+  margin: 0 1em;
+`
+
+class Layout extends React.Component {
   render() {
     const { location, children } = this.props
-    let header
-
-    let rootPath = `/`
-    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
-      rootPath = __PATH_PREFIX__ + `/`
-    }
+    const language = location.pathname.split('/')[1];
 
     return (
       <Container>
-        <Navigation />
+        <Navigation>
+          <NavigationItem>
+            <Link to={`/${language}`}>Home</Link>
+          </NavigationItem>
+        </Navigation>
         {children}
       </Container>
     )
   }
 }
 
-export default Template
+export default Layout
