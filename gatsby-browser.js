250c1525421bf6fd3678b054b1347d2c6bc7fb02
Ben Naylor <nayben@gmail.com>
2021-02-20 20:54:52 +0100

    chore: consistent exports

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,4 @@
 const wrapWithI18nProvider = require('./src/components/withI18n').wrapWithI18nProvider;
 exports.wrapPageElement = wrapWithI18nProvider;
 
-export function onServiceWorkerUpdateReady() { window.location.reload(true) };
+exports.onServiceWorkerUpdateReady = () => { window.location.reload(true) };
