8212048ae8db4ed46984a2961cabaa5666ad90ba
Ben Naylor <nayben@gmail.com>
2021-02-20 13:28:04 +0100

    fix: lazy fix to force SW to refresh if there is an update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,4 @@
 const wrapWithI18nProvider = require('./src/components/withI18n').wrapWithI18nProvider;
 exports.wrapPageElement = wrapWithI18nProvider;
+
+export function onServiceWorkerUpdateReady() { window.location.reload(true) };
